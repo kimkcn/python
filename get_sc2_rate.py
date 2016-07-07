353c792a1cf9d420ce9f7d0f0ecef8218ab07b6a
mflash <kimkcn01@163.com>
2016-07-07 13:23:35 +0800

    modify function named

--- get_sc2_rate.py
+++ get_sc2_rate.py
@@ -8,30 +8,30 @@ html = response.read()
 soup = BeautifulSoup(html)
 
 room_owner = soup.select('#live-list-contentbox > li > a > div > p > span.dy-name.ellipsis.fl')
-room_num = soup.select('#live-list-contentbox > li > a > div > p > span.dy-num.fr')
+room_rate = soup.select('#live-list-contentbox > li > a > div > p > span.dy-num.fr')
 room_title = soup.select('#live-list-contentbox > li > a > div > div > h3')
 info=[]
 
-for title,owner,num in zip(room_title,room_owner,room_num):
+for title,owner,num in zip(room_title,room_owner,room_rate):
     tmp=num.get_text()
     try:
-        room_num=int(tmp)
+        room_rate=int(tmp)
     except ValueError:
         if "." in tmp:
             data=''.join(re.findall('\d+\.?\d+',tmp))
-            room_num = int(float(data)*10000)
+            room_rate = int(float(data)*10000)
         else:
             data=''.join(re.findall('\d+',tmp))
-            room_num = int(data)*10000
+            room_rate = int(data)*10000
 
     total = [
         title.get_text(),
         owner.get_text(),
-        room_num
+        room_rate
     ]
     info.append(total)
 
 for i in info:
-    room_num = i[2]
-    if room_num > 10000:
+    room_rate = i[2]
+    if room_rate > 10000:
         print i[0],i[1],i[2]
